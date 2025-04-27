d802d5056f68f0f5d60ccf9b34d4bc6953c88074
AlinariC <acampbell@reedsport.k12.or.us>
2025-04-27 15:40:55 -0700

    Build script update
    Adjust PDF output size to 6x9"

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,7 +21,8 @@ pandoc Combined.md \
 # Build the Print PDF (no TOC)
 pandoc Combined.md \
   --pdf-engine=xelatex \
-  --variable geometry=margin=1in \
+  -V papersize=6in x 9in \
+  -V geometry=0.75in \
   --output="$OUTPUT_DIR/${TITLE// /_}-Print.pdf"
 
 # Cleanup
